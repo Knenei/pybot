b70cc52df03daae7c0eb403482438a2ffeb2ab84
Knenei <65310394+Knenei@users.noreply.github.com>
2020-06-01 14:47:29 -0400

    Update TheCatKingdom.py

--- TheCatKingdom.py
+++ TheCatKingdom.py
@@ -159,7 +159,7 @@ async def unban_error(ctx, error):
         await ctx.send('Missing Required Arguments.\n**Example**\nC.unban <@716138613783527505>')
 
 
-@client.command(aliases=['addrole', 'AR', 'Ar', 'aR', 'Addrole'])
+@client.command(aliases=['addrole', 'AR', 'Ar', 'aR', 'Addrole', 'ar'])
 @commands.has_permissions(manage_roles=True)
 async def add_role(ctx, user, *, role):
     await ctx.add_role(user, role)
@@ -174,7 +174,7 @@ async def add_role_error(ctx, error):
         await ctx.send('Missing required arguments.\n**Example**\nC.addrole <@716138613783527505> @(rolename)')
 
 
-@client.command(aliases=['removerole', 'RR', 'Rr', 'rR', 'Removerole'])
+@client.command(aliases=['removerole', 'RR', 'Rr', 'rR', 'Removerole', 'rr'])
 @commands.has_permissions(manage_roles=True)
 async def remove_role(ctx, user, *, role):
     await ctx.remove_role(user, role)
